174f838bc86a5084e219268578babbfe121c6d63
dsanthoshraj <36885729+dsanthoshraj@users.noreply.github.com>
2019-01-16 11:37:43 +0530

    Add files via upload

---
+++ edit_leads.php
@@ -0,0 +1,20 @@
+<?php
+
+	header("Content-type: application/xml");
+	$token="d8fb53012f18b2f0bab830d2d1fb31e2";
+	$id = "3758824000000198014";
+	$curl_url = "https://crm.zoho.com/crm/private/xml/Leads/getRecords";
+	$curl_auth_fields= "&id=".$id."&authtoken=".$token."&scope=crmapi";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $curl_url);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $curl_auth_fields);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result;
+	return $result;
+	
+?>
